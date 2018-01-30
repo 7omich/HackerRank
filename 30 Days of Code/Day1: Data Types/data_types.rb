85f92ce46316c896b93c2daff41820b6dbcbd30c
nb6u7 <b6u728@gmail.com>
2018-01-30 14:32:53 +0900

    Solve Data Types in Ruby

---
+++ 30 Days of Code/Day1: Data Types/data_types.rb
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i2 = gets.chomp.to_i
+d2 = gets.chomp.to_f
+s2 = gets.chomp
+# Print the sum of both integer variables on a new line.
+print "#{i + i2}\n"
+# Print the sum of the double variables on a new line.
+print "#{d + d2}\n"
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print "#{s + s2}\n"
