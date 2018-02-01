3a02c96f3464e98af238aa67d872826705f92e59
nb6u7 <b6u728@gmail.com>
2018-02-02 00:03:57 +0900

    Solve Operators in Java

---
+++ 30 Days of Code/Day02: Operators/operators.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    double meal_cost = in.nextDouble();
+    int tip_percent = in.nextInt();
+    int tax_percent = in.nextInt();
+    in.close();
+
+    double total_cost = meal_cost * (100 + tip_percent + tax_percent) / 100;
+    long rounded_cost = Math.round(total_cost);
+    String answer = String.format("The total meal cost is %s dollars.", rounded_cost);
+    System.out.println(answer);
+  }
+}
